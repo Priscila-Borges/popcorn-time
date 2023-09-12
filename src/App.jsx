a2fd53d79a747ee5b716acc17f92c4dbe59f2b1a
Priscila Borges <priscila.borges.nl@gmail.com>
2023-09-12 12:05:38 +0200

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+     <Header />
+     <Main />
+     <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Main.jsx
@@ -0,0 +1,5 @@
+function Main(){
+    return <h1>This is the Main component</h1>
+}
+
+export default Main;
